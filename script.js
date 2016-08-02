0ab6428e87b092cef5a02ec53e5706ba51497bb8
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-08-02 18:10:40 -0400

    Updates added from API Object information

--- script.js
+++ script.js
@@ -18,20 +18,34 @@ var context = canvas.getContext('2d');
 				console.log(weatherData);
 				//set up a variable for the user's city temp
 				currentTemp = weatherData.list[0].main.temp;
-			
+				currentMax = weatherData.list[0].main.temp_max;
+				currentMin = weatherData.list[0].main.temp_min;
+				weatherIcon = weatherData.list[0].weather[0].icon;
+				weatherDescription = weatherData.list[0].weather[0].description;
+				console.log(weatherDescription)
+				$('.maxTemp').html("<span>Max Temp</span> <br>"+currentMax + ' F');
+				//$(currentMax).css("{}");
+
+				$('.minTemp').html("<span>Min Temp</span> <br>"+currentMin + ' F');
+				//$(currentMax).css("{}");
+
+				$('.description').html("<span>Current Conditions</span> <br>"+weatherDescription);
+
+				var src1 = "http://openweathermap.org/img/w/"+weatherIcon+".png";
+				$('.weather-icon').attr("src", src1);
+
 				animate(0);
 			});
 		});
-			function animate(current){
-				var tempColor = '#fff';
-				context.strokeStyle = tempColor;
-				context.lineWidth = 10;
 
-			
+			function animate(current){
+				var tempColor = '#ff5964';
+				context.strokeStyle = tempColor;
+				context.lineWidth = 15;	
 
 			context.beginPath();
 			context.arc(155,155,60,Math.PI * 1.5, Math.PI * 1.5, Math.PI* 3);
-			context.fillStyle = '#fff';
+			context.fillStyle = '#ffffff';
 			context.fill(); 
 
 			//clear out the canvas
@@ -44,7 +58,9 @@ var context = canvas.getContext('2d');
 			//drar that circle!
 			context.stroke();
 
-			context.fillText(currentTemp, 100, 100);
+			context.font="35px fantasy";
+			context.fillText(currentTemp+" F", 110, 170);
+
 
 			current++;
 			if(current < currentTemp){
